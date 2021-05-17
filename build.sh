af9a5cfde8430f0c0272ff7e4a8add1677ca6f75
xawlw <abdulazizawlw@gmail.com>
2021-05-17 16:34:26 +0300

    Etherious v2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export PATH="$HOME/tc/proton-clang/bin:$PATH"
 SECONDS=0
-ZIPNAME="Etherious-LA.UM.9.x-vayu-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="Etherious-v2-vayu-$(date '+%Y%m%d-%H%M').zip"
 
 if ! [ -d "$HOME/tc/proton-clang" ]; then
 echo "Proton clang not found! Cloning..."
